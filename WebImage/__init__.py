3f41c8976119d55a66b061a0be4ffe2a6428858e
NOLRAM-MYM <white.as.snow.ws@gmail.com>
2024-06-01 21:34:43 +0900

    fotos

--- WebImage/__init__.py
+++ WebImage/__init__.py
@@ -13,8 +13,8 @@ app = Flask(__name__) #cliar uma aplicação
 # else:
 #     link_BD = "sqlite://comunidade.db"
 # somente uma vez para criar as tabelas do banco de dados no render
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://banco_webimage_user:UYlGav9KGWuYrpCKdkxrsQkGTVziH8C9@dpg-cpdfo4dds78s73egq480-a.oregon-postgres.render.com/banco_webimage"
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") #conrigurar dentro do render database o nome do link interno
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://banco_webimage_user:UYlGav9KGWuYrpCKdkxrsQkGTVziH8C9@dpg-cpdfo4dds78s73egq480-a.oregon-postgres.render.com/banco_webimage"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") #conrigurar dentro do render database o nome do link interno
 app.config["SECRET_KEY"] = "c179ad01ab8a2855afd8724d01d43286" #senha de criptografia do site
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts" # configuraçao para receber uploads de arquivos ou nesse caso fotos
 #app.config["UPLOAD_FOLDER"] = "/fotos_posts"
